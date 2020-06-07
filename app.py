4d5148c80aa9afada08016e6745f02deda1cbd5d
Seiya Kobayashi <seiyak@bu.edu>
2020-06-08 02:59:02 +0900

    Fixed bugs.

--- app.py
+++ app.py
@@ -40,55 +40,63 @@ def callback():
 @handler.add(FollowEvent)
 def message_init(event):
     line_id = line_bot_api.get_profile(event.source.user_id).user_id
-    db.session.add(User(line_id=line_id))
-    db.session.commit()
+    if User.query.filter_by(line_id=line_id).first():     # If user account already exists (i.e., past user)
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text='友達再追加ありがとうございます！'),
+            ]
+        )
+    else:     # If new user
+        db.session.add(User(line_id=line_id))
+        db.session.commit()
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        [
-            TextSendMessage(text='友達追加ありがとうございます！\niHack 公式LINE botです！'),
-            TextSendMessage(text='LINE botはあなたのことをなんとお呼びすればよいですか？\nお名前またはニックネームを教えてください。')
-        ]
-    )
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text='友達追加ありがとうございます！'),
+                TextSendMessage(text='あなたのことをなんとお呼びすればよいですか？\nお名前またはニックネームを教えてください。')
+            ]
+        )
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+    print('event:', event)
     user = User.query.filter_by(line_id=line_bot_api.get_profile(event.source.user_id).user_id).first()
-    # name
-    if user.init_step == 0:
-        line_bot_api.reply_message(
-            event.reply_token,
-            [
-                TextSendMessage(text='ありがとうございます。\nお名前を「'+event.message.text+'」と設定しました。'),
-                TextSendMessage(text='続いて、アカウントの作成をしていきましょう。\nメールアドレスを入力してください。')
-            ]
-        )
-    # email
-    elif user.init_step == 1:
-        line_bot_api.reply_message(
-            event.reply_token,
-            [
-                TextSendMessage(text='メールアドレスを「'+event.message.text+'」に設定しました。'),
-                TextSendMessage(text='続いて、アカウントのパスワードを設定してください。')
-            ]
-        )
-    # password
-    elif user.init_step == 2:
-        line_bot_api.reply_message(
-            event.reply_token,
-            [
-                TextSendMessage(text='パスワードを'+event.message.text+'」に設定しました。'),
+    if init_step <= 3:     # If initial setup is done
+        if user.init_step == 0:     # Ask name
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=event.message.text+'さん、こんばんは！'),
+                    TextSendMessage(text='それでは、アカウントの作成をしていきましょう。\nよく使用するメールアドレスを入力してください。')
+                ]
+            )
+        elif user.init_step == 1:     # Ask email
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text='メールアドレスを '+event.message.text+' に設定しました。'),
+                    TextSendMessage(text='続いて、アカウントのパスワードを設定してください。')
+                ]
+            )
+        elif user.init_step == 2:     # Ask to set password
+            line_bot_api.reply_message(
+                event.reply_token,
                 TextSendMessage(
-                    text=
-                        'これで初期設定は完了です！\n\nこのLINE botでは以下の設定や確認ができます。\n各種設定を行いたい => 「設定」と入力\nフィードバック => 「FB」と入力'
+                    text='これで初期設定は完了です！\n\n今後の操作方法は以下をご確認ください。\n各種設定: 「設定」と入力'
                 )
-            ]
+            )
+        user.init_step += 1
+        db.session.commit()
+    else:     # If initial setup is not done
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text='こんばんは。'
+            )
         )
-
-    user.init_step += 1
-    db.session.commit()
-
 
 if __name__ == "__main__":
     app.run()
