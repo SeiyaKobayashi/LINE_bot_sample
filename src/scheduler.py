74f87982cd1bfc5fcf4ebc58e0ab96840d3ad273
Seiya Kobayashi <seiyak@bu.edu>
2020-06-20 23:53:05 +0900

    debugging.

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,10 +2,11 @@
 # scheduler.py
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from src.app import push_weather_forecast
+from src.app import app.push_weather_forecast
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    # scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
-    scheduler.add_job(push_weather_forecast, 'interval', minutes=2)
-    scheduler.start()
+    with app.app_context():
+        scheduler = BlockingScheduler()
+        # scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
+        scheduler.add_job(push_weather_forecast, 'interval', minutes=2)
+        scheduler.start()
