d886edbcd763be21e2905d7ea31de47f61a7f716
Seiya Kobayashi <seiyak@bu.edu>
2020-06-21 07:15:29 +0900

    debugging.

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,17 +2,24 @@
 # scheduler.py
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from src.app import push_weather_forecast, push_daily_reminder_fetch_users, push_daily_reminder
 
-if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    users = push_daily_reminder_fetch_users()
+
+def catchUpdates(users=push_daily_reminder_fetch_users()):
     for user in users:
         print('name:', user.name)
         hour = user.default_time.split(':')[0] if user.default_time.split(':')[0][0] != '0' else user.default_time.split(':')[0][1]
         minute = user.default_time.split(':')[1] if user.default_time.split(':')[1][0] != '0' else user.default_time.split(':')[1][1]
         print('hour:', hour)
         print('minute:', minute)
-        scheduler.add_job(push_daily_reminder, 'cron', hour=hour, minute=minute, args=[user])
-    scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
-    scheduler.start()
+        bl_scheduler.add_job(push_daily_reminder, args=[user], 'cron', hour=hour, minute=minute)
+
+
+if __name__ == '__main__':
+    bl_scheduler = BlockingScheduler()
+    bl_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
+    bg_scheduler = BackgroundScheduler()
+    bg_scheduler.add_job(catchUpdates, 'cron', minute=*)
+    bl_scheduler.start()
+    bg_scheduler.start()
