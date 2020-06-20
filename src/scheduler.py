d4f03997c8b40dac97372a069220c2293509073e
Seiya Kobayashi <seiyak@bu.edu>
2020-06-21 07:17:19 +0900

    debugging.

--- src/scheduler.py
+++ src/scheduler.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     bl_scheduler = BlockingScheduler()
     bl_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
     bg_scheduler = BackgroundScheduler()
-    bg_scheduler.add_job(catchUpdates, 'cron', minute=*)
+    bg_scheduler.add_job(catchUpdates, 'cron', minute='*')
     bl_scheduler.start()
     bg_scheduler.start()
