98ad3634674e9e7d33f3cb800f08f38e92b1ab89
Seiya Kobayashi <seiyak@bu.edu>
2020-06-21 08:10:15 +0900

    debugging.

--- src/scheduler.py
+++ src/scheduler.py
@@ -14,15 +14,14 @@ def catchUpdates():
         minute = user.default_time.split(':')[1] if user.default_time.split(':')[1][0] != '0' else user.default_time.split(':')[1][1]
         print('hour:', hour)
         print('minute:', minute)
-        bl_scheduler.add_job(push_daily_reminder, 'cron', hour=hour, minute=minute, args=[user])
+        bg_scheduler.add_job(push_daily_reminder, 'cron', hour=hour, minute=minute, args=[user])
 
 
 if __name__ == '__main__':
-    bl_scheduler = BlockingScheduler()
-    bl_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
-    bl_scheduler.start()
-    bl_scheduler.print_jobs()
+    # bl_scheduler = BlockingScheduler()
+    # bl_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
+    # bl_scheduler.start()
     bg_scheduler = BackgroundScheduler()
+    bg_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
     bg_scheduler.add_job(catchUpdates, 'interval', seconds=10)
     bg_scheduler.start()
-    bg_scheduler.print_jobs()
