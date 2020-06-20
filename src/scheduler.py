f7ec0de7d597f54a450cbfff021775738f655a6f
Seiya Kobayashi <seiyak@bu.edu>
2020-06-21 08:21:57 +0900

    debugging.

--- src/scheduler.py
+++ src/scheduler.py
@@ -13,16 +13,13 @@ if __name__ == '__main__':
     # bl_scheduler.start()
     bl_scheduler = BlockingScheduler()
     bl_scheduler.add_job(push_weather_forecast, 'cron', hour='0, 6, 12')
-    
-    while True:
-        users = push_daily_reminder_fetch_users()
-        for user in users:
-            print('name:', user.name)
-            hour = user.default_time.split(':')[0] if user.default_time.split(':')[0][0] != '0' else user.default_time.split(':')[0][1]
-            minute = user.default_time.split(':')[1] if user.default_time.split(':')[1][0] != '0' else user.default_time.split(':')[1][1]
-            print('hour:', hour)
-            print('minute:', minute)
-            bl_scheduler.add_job(push_daily_reminder, 'cron', hour=hour, minute=minute, args=[user])
-        sleep(20)
+
+    # while True:
+    #     users = push_daily_reminder_fetch_users()
+    #     for user in users:
+    #         hour = user.default_time.split(':')[0] if user.default_time.split(':')[0][0] != '0' else user.default_time.split(':')[0][1]
+    #         minute = user.default_time.split(':')[1] if user.default_time.split(':')[1][0] != '0' else user.default_time.split(':')[1][1]
+    #         bl_scheduler.add_job(push_daily_reminder, 'cron', hour=hour, minute=minute, args=[user])
+    #     sleep(30)
 
     bl_scheduler.start()
